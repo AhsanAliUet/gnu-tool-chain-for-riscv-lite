da82c456e5dccc1995bbaacb9735779a81c4d9c2
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-10-30 23:18:48 +0500

    Added Makefile for ease and some more minor changes :)

--- Example/make_a_to_m.bat
+++ Example/make_a_to_m.bat
@@ -2,8 +2,8 @@
 ::riscv64-unknown-elf-as -c -o build/startup.o src/startup.s -march=rv32i -mabi=ilp32
 rmdir -r build
 mkdir build
-riscv64-unknown-elf-as -c -o build/fact.o src/fact.s -march=rv32i -mabi=ilp32
-riscv64-unknown-elf-gcc -o build/fact.elf build/fact.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
-riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/fact.elf build/fact.bin
-python3 maketxt.py build/fact.bin > build/fact.txt
-riscv64-unknown-elf-objdump -S -s build/fact.elf > build/fact.dump
+riscv64-unknown-elf-as -c -o build/r_type.o src/r_type.s -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-gcc -o build/r_type.elf build/r_type.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/r_type.elf build/r_type.bin
+python3 maketxt.py build/r_type.bin > build/r_type.txt
+riscv64-unknown-elf-objdump -S -s build/r_type.elf > build/r_type.dump
---
+++ Example/src/r_type.s
@@ -0,0 +1,14 @@
+addi x1, x0, 1
+addi x2, x0, 2
+addi x3, x0, 3
+addi x4, x0, 4
+addi x5, x0, 5
+addi x6, x0, 6
+
+add x1, x1, x1
+sub x2, x0, x2
+or x8, x1, x1
+and x9, x9, x0
+
+sw x1, 10(x0)
+lw x10, 10(x0)
